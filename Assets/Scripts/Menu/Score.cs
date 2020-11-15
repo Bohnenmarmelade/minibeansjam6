4e103da18eb10031c5d32af78bc440ed65f28e35
Jenny Neuhard <j.neuhard@gmx.de>
2020-11-15 12:29:09 +0100

    Added start menu and game over Added restarting the game

---
+++ Assets/Scripts/Menu/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : Singleton<Score>
+{
+    public int levels = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = 0;
+        EventManager.Instance.OnLevelFinished.AddListener(UpdateLevelScore);
+        EventManager.Instance.OnPlayerDeath.AddListener(OnPlayerDeath);
+    }
+
+    private void UpdateLevelScore()
+    {
+        levels++;
+    }
+
+    private void OnPlayerDeath()
+    {
+        levels--;
+        SceneManager.LoadScene(2);
+    }
+    
+    
+}
---
+++ Assets/Scripts/Menu/ScoreView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreView : MonoBehaviour
+{
+    public Text text;
+    private int level = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = Score.Instance.levels;
+        text.text = "You created " + level + " new whale worlds!";
+    }
+
+    public void StartNewWorld()
+    {
+        level = 0;
+        Score.Instance.levels = 0;
+        SceneManager.LoadScene(1);
+    }
+}
